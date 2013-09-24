7fdabcc347248f10a38f3fbb09629ad51ec48a0f
goto100 <goto100@gmail.com>
2013-09-24 15:58:41 +0800

    register

--- test/register.js
+++ test/register.js
@@ -1,24 +1,25 @@
-describe('registry', function() {
+describe('register', function() {
 
 	it('template', function() {
 		var A = new Class(ui.Component, {
 			meta: {
 				tag: 'mytag',
-				template: '<div class="shadow"></div>'
+				template: '<div class="shadow"><content select="span"></content></div>'
 			}
 		});
 
-		var context = S.one('<div><mytag></mytag></div>');
+		var context = S.one('<div><x-mytag><span>1</span><div>2</div><span>3</span></x-mytag></div>');
 		ui.bootstrap(context);
-		var component = context.one('mytag')[0].component;
+		var component = context.one('x-mytag')[0].component;
 
-		console.log(component.node.html());
+		assert.equal(component.node.html(), '<div class="shadow"><span>1</span><span>3</span></div>');
 	});
 
 	it('bootstrapped', function() {
 		var inited = false;
 		var A = new Class(ui.Component, {
 			meta: {
+				namcespace: 'x',
 				tag: 'mytag'
 			},
 			initialize: function(node) {
@@ -27,10 +28,32 @@ describe('registry', function() {
 			}
 		});
 
-		var context = S.one('<div><mytag></mytag></div>');
+		var context = S.one('<div><x-mytag></x-mytag></div>');
 		ui.bootstrap(context);
 
 		assert.equal(inited, 1);
+	});
+
+	it('namespace', function() {
+		var A = new Class(ui.Component, {
+			meta: {
+				tag: 'mytag'
+			}
+		});
+
+		var B = new Class(ui.Component, {
+			meta: {
+				namespace: 'my',
+				tag: 'tag'
+			}
+		});
+
+		var context = S.one('<div><x-mytag class="mytag" /><my-tag class="mytag2" /><mytag class="mytag3" /></div>');
+		ui.bootstrap(context);
+
+		assert.ok(context.one('.mytag')[0].component);
+		assert.ok(context.one('.mytag2')[0].component);
+		assert.ok(!context.one('.mytag3')[0].component);
 	});
 
 });
