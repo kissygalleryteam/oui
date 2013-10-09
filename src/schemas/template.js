0da1f467dc6c5e0b8c48131178c3ecf2ead96bd9
goto100 <goto100@gmail.com>
2013-10-09 17:55:30 +0800

    compatible templating

--- src/schemas/template.js
+++ src/schemas/template.js
@@ -1,10 +1,26 @@
 KISSY.add(function(S, oop, promise, Handler, dom, Mustache) {
 
 	var TemplateHandler = new oop.Class(Handler, {
+		getTemplate: function(options) {
+			options = options || {};
 
+			var template;
+			if (options['template']) {
+				template = options['template'];
+			} else if (options['template-from']) {
+	        	template = S.one(options['template-from']).html();
+			} else if (options['template-module']) {
+				template = S.require(options['template-module']);
+			}
+			return template;
+		},
+		getRenderPoint: function(options) {
+	        return S.one(options['template-from']);
+		},
 		renderShadow: function(component) {
+			var self = this;
 			var shadow, nodes, placehoders;
-			var template = component.meta.template;
+			var template = self.getTemplate(component.meta);
 			if (template) {
 				shadow = document.createDocumentFragment();
 				S.all(template).appendTo(shadow);
@@ -24,12 +40,14 @@ KISSY.add(function(S, oop, promise, Handler, dom, Mustache) {
 			}
 		},
 		handleNew: function(metaclass, name, base, dict) {
+			var self = this;
 	        var func = function(name, data, callback) {
-	        	var templateNode = S.one(this.__properties__[name].options['template-from']);
-	        	var template = templateNode.html();
+	        	var member = this.__properties__[name];
+	        	var template = self.getTemplate(member);
 	        	var result = Mustache.to_html(template, data);
-	        	if (templateNode) {
-	        		dom.insertBefore(S.one(result), templateNode);
+	        	var point = self.getRenderPoint(member);
+	        	if (point) {
+	        		dom.insertBefore(S.one(result), point);
 	        	} else {
 	        		this.node.append(result);
 	        	}
@@ -50,5 +68,5 @@ KISSY.add(function(S, oop, promise, Handler, dom, Mustache) {
 	};
 
 }, {
-	requires: ['gallery/oop/0.1/index', './promise', '../Handler', 'dom', 'brix/gallery/mu/index']
+	requires: ['gallery/oop/0.1/index', './promise', '../Handler', 'dom', 'brix/gallery/mu/index', 'sizzle']
 })
