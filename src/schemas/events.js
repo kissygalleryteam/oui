6446af52ca1d73f1cc8ec05b4f65c8734c93bd95
goto100 <goto100@gmail.com>
2013-10-30 15:35:03 +0800

    type

--- src/schemas/events.js
+++ src/schemas/events.js
@@ -65,7 +65,7 @@ var SubEventHandler = new Class(Handler, {
                         }
                     });
                 });
-        }
+            }
         });
 
         subEvents.forEach(function(item) {
