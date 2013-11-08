85b316bc851f944531d36afaedd8d9d5ae404459
goto100 <goto100@gmail.com>
2013-11-08 09:18:51 +0800

    immediate option for time.interval

--- src/schemas/time.js
+++ src/schemas/time.js
@@ -55,10 +55,16 @@ function debounce(wait, immediate) {
     };
 };
 
-function interval(time) {
+function interval(time, immediate) {
     return function(func) {
         var member = function() {
-            member.timer = setInterval(S.bind(func, this), member.time);
+            var obj = this, args = arguments;
+            if (immediate) {
+                func.apply(this, arguments);
+            }
+            member.timer = setInterval(function() {
+                func.apply(obj, args);
+            }, member.time);
         };
         member.time = time;
         member.stop = function() {
